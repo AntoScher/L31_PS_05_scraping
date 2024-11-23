21e1d46c3eab33055db0e72afd49a7d26278c968
AntoScher <asc8er@gmail.com>
2024-11-23 18:49:57 +0300

    agane

--- divanpars/divanpars/spiders/divannewpars.py
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -1,10 +1,32 @@
 import scrapy
+from scrapy.crawler import CrawlerProcess
 
-
-class DivannewparsSpider(scrapy.Spider):
-    name = "divannewpars"
-    allowed_domains = ["https://www.divan.ru"]
-    start_urls = ["https://www.divan.ru/category/svet"]
+class DivanSpider(scrapy.Spider):
+    name = "divan"
+    start_urls = [
+        'https://www.divan.ru/category/svet',
+    ]
 
     def parse(self, response):
-        pass
+        print(f"Visited {response.url}")
+        for product in response.css('div.Pk6w8'):
+            name = product.css('img::attr(alt)').get()
+            if name:
+                print(f"Found product: {name}")
+            yield {
+                'name': name,
+            }
+
+if __name__ == "__main__":
+    process = CrawlerProcess()
+    process.crawl(DivanSpider)
+    process.start()
+    print("Crawling finished")
+
+
+
+      #response.css('div.Pk6w8 img::attr(alt)').get()
+
+
+
+        
